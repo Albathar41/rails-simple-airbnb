7e4132350f1554f84b246a7122a9b71c1249280d
Maxime Moreira <maxime.moreira@gmail.com>
2023-05-25 10:36:27 +0200

    cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#clean the DB
+Flat.destroy_all
+
+puts "#{Flat.all.count} flat in the DB"
+
+14.times do
+  Flat.create!(
+    name: Faker::DcComics.title,
+    address: Faker::Address.full_address,
+    description: Faker::Quotes::Rajnikanth.joke,
+    price_per_night: rand(50..250),
+    number_of_guests: rand(1..10)
+  )
+end
+
+puts "#{Flat.all.count} flats created"
